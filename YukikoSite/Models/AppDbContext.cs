3ee895a247554687a3f553eaf5086f9535ef9600
Дмитрий Мартынов <makak2009@mail.ru>
2021-05-28 13:58:05 +0700

    Preset db data

--- YukikoSite/Models/AppDbContext.cs
+++ YukikoSite/Models/AppDbContext.cs
@@ -29,6 +29,22 @@ namespace YukikoSite.Models {
             };
             modelBuilder.Entity<Role>().HasData(new Role[] { adminRole });
             modelBuilder.Entity<User>().HasData(new User[] { adminUser });
+            modelBuilder.Entity<GlovesItem>().HasData(DataPreset(new GlovesItem(), "gloves"));
+            modelBuilder.Entity<OthersItem>().HasData(DataPreset(new OthersItem(), "other"));
+            modelBuilder.Entity<SidingItem>().HasData(DataPreset(new SidingItem(), "siding"));
+            modelBuilder.Entity<VentilationItem>().HasData(DataPreset(new VentilationItem(), "ventil"));
+        }
+
+        private T[] DataPreset<T>(T item, string name) where T : IModelItem {
+            T[] data = new T[3];
+            for(int i = 1; i <= 3; i++) {
+                var el = data[i - 1] = (T)Activator.CreateInstance(typeof(T));
+                el.Id = i;
+                el.ImagePath = $"{name}{i}.jpg";
+                el.Title = $"{name}{i}";
+                el.Description = $"This is {name}{i}";
+            }
+            return data;
         }
     }
 }
