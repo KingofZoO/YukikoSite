286f8e4fe51be9dec2c615cda0dcadcbea12d0d7
Дмитрий Мартынов <makak2009@mail.ru>
2021-06-16 17:42:38 +0700

    Adding machine key for hosting on a web farm

--- YukikoSite/Startup.cs
+++ YukikoSite/Startup.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,12 +17,17 @@ using YukikoSite.Models;
 namespace YukikoSite {
     public class Startup {
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment hostingEnvironment;
 
-        public Startup(IConfiguration configuration) {
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment) {
             Configuration = configuration;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         public void ConfigureServices(IServiceCollection services) {
+            var keysFolder = Path.Combine(hostingEnvironment.ContentRootPath, "Keys");
+            services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(keysFolder)).SetApplicationName("yukiko");
+
             string connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<AppDbContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(10, 5, 8))));
 
